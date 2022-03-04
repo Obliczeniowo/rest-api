f3d9908a494bac40e4d9d135a4db2eeaedc3f7f8
Obliczeniowo <zajaczkowskikrzysztofkz@gmail.com>
2022-03-04 03:23:32 +0100

    add basic rout and contoller

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
 const express = require('express');
 
+const feedRoutes = require('./controllers/feed.controllers.js');
+
 const app = express();
+
+app.use(feedRoute, feedRoutes);
 
 app.listen(8080);
 
