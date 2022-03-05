fd39dd58545b1fdff32cb12735003e7550bf8e06
Obliczeniowo <zajaczkowskikrzysztofkz@gmail.com>
2022-03-05 09:54:44 +0100

    basic rout for login

--- controllers/auth.controllers.js
+++ controllers/auth.controllers.js
@@ -4,6 +4,7 @@ const bcrypt = require('bcrypt');
 const { validationResult } = require('express-validator');
 
 const { errorCb, getError } = require('../utils/errors.js');
+const { is } = require('express/lib/request');
 
 exports.signup = (req, res, next) => {
   const errors = validationResult(req);
@@ -15,18 +16,39 @@ exports.signup = (req, res, next) => {
   bcrypt
     .hash(req.body.password, 12)
     .then((password) => {
-      const user = new User(
-          {
-              ...req.body,
-              password: password
-          }
-      );
+      const user = new User({
+        ...req.body,
+        password: password,
+      });
 
       return user.save();
-    }).then(result => {
-        res.status(201).json({ userId: result._id })
+    })
+    .then((result) => {
+      res.status(201).json({ userId: result._id });
     })
     .catch((error) => {
       return errorCb(error);
+    });
+};
+
+exports.login = (req, res, next) => {
+  let loadedUser;
+  User.findOne({ email: req.body.email })
+    .then((user) => {
+      if (!user) {
+        throw getError('Email cant be find', 401);
+      }
+
+      loadedUser = user;
+
+      bcrypt.compare(req.body.password, user.password);
+    })
+    .then(isEqual => {
+        if (!isEqual) {
+            throw getError('Bad password', 401);
+        }
+    })
+    .catch((error) => {
+      errorCb(error);
     });
 };
