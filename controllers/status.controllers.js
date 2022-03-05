11348085f15058ca3872d51b0efd32f280a729d0
Obliczeniowo <zajaczkowskikrzysztofkz@gmail.com>
2022-03-05 14:29:19 +0100

    async await

--- controllers/status.controllers.js
+++ controllers/status.controllers.js
@@ -4,37 +4,33 @@ const { validationResult } = require('express-validator');
 
 const { errorCb, getError } = require('../utils/errors.js');
 
-exports.getStatus = (req, res, next) => {
-  return User.findById(req.userId)
-    .then((user) => {
-      if (!user) {
-        throw getError('User not exist');
-      }
-      res.status(200).json({ status: user.status });
-    })
-    .catch((err) => {
-      return errorCb(err, next);
-    });
+exports.getStatus = async (req, res, next) => {
+  try {
+    const user = await User.findById(req.userId);
+    if (!user) {
+      throw getError('User not exist');
+    }
+    res.status(200).json({ status: user.status });
+  } catch (err) {
+    return errorCb(err, next);
+  }
 };
 
-exports.updateStatus = (req, res, next) => {
+exports.updateStatus = async (req, res, next) => {
   const errors = validationResult(req);
   if (!errors.isEmpty()) {
     throw getError('Something went wrong!', 422);
   }
 
-  return User.findById(req.userId)
-    .then((user) => {
-      if (!user) {
-        throw getError('User not exist');
-      }
-      user.status = req.body.status;
-      return user.save();
-    })
-    .then(() => {
-      res.status(200).json({ message: 'Updated' });
-    })
-    .catch((err) => {
-      return errorCb(err, next);
-    });
+  try {
+    const user = await User.findById(req.userId);
+    if (!user) {
+      throw getError('User not exist');
+    }
+    user.status = req.body.status;
+    await user.save();
+    res.status(200).json({ message: 'Updated' });
+  } catch (err) {
+    return errorCb(err, next);
+  }
 };
