919f31de86284e3bfc1ff4c4845201a0b944b8d1
Obliczeniowo <zajaczkowskikrzysztofkz@gmail.com>
2022-03-05 14:50:33 +0100

    configure socket connection

--- app.js
+++ app.js
@@ -64,6 +64,11 @@ app.use((error, req, res, next) => {
 mongoose
   .connect(mongodbUrl, { dbName: 'messages' })
   .then(() => {
-    app.listen(8080);
+    const server = app.listen(8080);
+
+    const io = require('socket.io')(server);
+    io.on('connection', socket => {
+        console.log('Client connected');
+    })
   })
   .catch((err) => console.log(err));
