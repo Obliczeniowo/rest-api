3aa856d4c0fd10736d4807cb35dabcdd199ae5b2
Obliczeniowo <zajaczkowskikrzysztofkz@gmail.com>
2022-03-05 02:34:09 +0100

    headers and resources

--- app.js
+++ app.js
@@ -7,12 +7,15 @@ const app = express();
 
 app.use(bodyParser.json());
 
-app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE');
-    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization,');
+app.use( (req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-requested-With, Content-Type, Accept");
+    if(req.method === "OPTIONS"){
+      res.header("Access-Control-Allow-Methods", "GET, POST, DELETE");
+      return res.status(200).json({});
+    }
     next();
-})
+  });
 
 app.use('/feed',feedRoutes);
 
